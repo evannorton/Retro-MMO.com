e3ca6a51a8549b3af29caad8a5b496e0bbba8f8e
evan <evanan39@yahoo.com>
2020-08-21 00:59:58 -0400

    don't use innerHTML for leaderboards

--- leaderboards.js
+++ leaderboards.js
@@ -2,11 +2,15 @@ fetch("https://play.retro-mmo.com/leaderboards.json").then((res) => res.json()).
     const tbody = document.querySelector("table tbody");
     rows.forEach((row, key) => {
         const tr = document.createElement("tr");
-        tr.innerHTML = `
-            <th>${key + 1}</th>
-            <th>${row.username}</th>
-            <th>${row.experience}</th>
-        `;
+        const th1 = document.createElement("th");
+        th1.innerText = `${key + 1}`;
+        const th2 = document.createElement("th");
+        th2.innerText = `${row.username}`;
+        const th3 = document.createElement("th");
+        th3.innerText = `${row.experience}`;
+        tr.appendChild(th1);
+        tr.appendChild(th2);
+        tr.appendChild(th3);
         tbody.appendChild(tr);
     });
     document.getElementById("leaderboards").removeAttribute("hidden");
