a225887d67d4edda86156ffc59924cc25918f85a
evan <evanan39@yahoo.com>
2020-08-28 01:31:40 -0400

    password resets

---
+++ reset-password.js
@@ -0,0 +1,24 @@
+document.getElementById("reset-password-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const token = new URLSearchParams(window.location.search).get("token");
+    fetch("https://retro-mmo.com/reset-password", {
+        body: JSON.stringify({
+            password: document.getElementById("password").value,
+            token
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST"
+    }).then(({ status }) => {
+        switch (status) {
+            case 200:
+                alert("Your password has been reset!");
+                break;
+            case 401:
+                alert("Your password reset link is invalid or has expired.");
+                break;
+        }
+        location.replace("./");
+    });
+});
