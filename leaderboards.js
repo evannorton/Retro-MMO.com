19db25d160a926e58ad5b2f51044be67d35a6e9a
Evan Norton <evanan39@yahoo.com>
2020-10-07 11:19:21 -0400

    format integers

--- leaderboards.js
+++ leaderboards.js
@@ -3,11 +3,11 @@ fetch("https://play.retro-mmo.com/leaderboards.json").then((res) => res.json()).
     rows.forEach((row, key) => {
         const tr = document.createElement("tr");
         const th1 = document.createElement("th");
-        th1.innerText = `${key + 1}`;
+        th1.innerText = `${(key + 1).toLocaleString()}`;
         const th2 = document.createElement("th");
         th2.innerText = `${row.username}`;
         const th3 = document.createElement("th");
-        th3.innerText = `${row.experience}`;
+        th3.innerText = `${row.experience.toLocaleString()}`;
         tr.appendChild(th1);
         tr.appendChild(th2);
         tr.appendChild(th3);
