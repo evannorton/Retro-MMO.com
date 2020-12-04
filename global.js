a86ce9da540c6b5e5b489c8b7cce3ee21c562240
evan <evanan39@yahoo.com>
2020-12-04 08:50:30 -0500

    format player count

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
 fetch("https://play.retro-mmo.com/players.json").then((res) => res.json()).then((players) => {
     const one = players.length === 1;
-    document.getElementById("players").innerText = `There ${one ? "is" : "are"} currently ${players.length} player${one ? "" : "s"} online!`;
+    document.getElementById("players").innerText = `There ${one ? "is" : "are"} currently ${players.length.toLocaleString()} player${one ? "" : "s"} online!`;
 });
