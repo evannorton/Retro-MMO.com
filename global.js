c5f96656f2ba86eeb003616c910d44b6606920de
evan <evanan39@yahoo.com>
2020-12-04 08:46:56 -0500

    remove console.log

--- global.js
+++ global.js
@@ -1,5 +1,4 @@
 fetch("https://play.retro-mmo.com/players.json").then((res) => res.json()).then((players) => {
     const one = players.length === 1;
     document.getElementById("players").innerText = `There ${one ? "is" : "are"} currently ${players.length} player${one ? "" : "s"} online!`;
-    console.log(players.length);
 });
