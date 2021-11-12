e423b9848e816e6162a40d8583476b3543cf479c
Evan Norton <evan@retro-mmo.com>
2021-11-12 08:53:07 -0500

    use td element

--- leaderboards.js
+++ leaderboards.js
@@ -2,15 +2,15 @@ fetch("https://play.retro-mmo.com/leaderboards.json").then((res) => res.json()).
     const tbody = document.querySelector("table tbody");
     rows.forEach(({ experience, username }, key) => {
         const tr = document.createElement("tr");
-        const th1 = document.createElement("th");
-        th1.innerText = (key + 1).toLocaleString();
-        const th2 = document.createElement("th");
-        th2.innerText = username;
-        const th3 = document.createElement("th");
-        th3.innerText = experience.toLocaleString();
-        tr.appendChild(th1);
-        tr.appendChild(th2);
-        tr.appendChild(th3);
+        const td1 = document.createElement("td");
+        td1.innerText = (key + 1).toLocaleString();
+        const td2 = document.createElement("td");
+        td2.innerText = username;
+        const td3 = document.createElement("td");
+        td3.innerText = experience.toLocaleString();
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
         tbody.appendChild(tr);
     });
     document.getElementById("leaderboards").removeAttribute("hidden");
