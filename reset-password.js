891b1d29293d5dca3e948278c13f4515b7f3db18
Evan Norton <evan@retro-mmo.com>
2022-01-08 08:18:02 -0500

    fix url

--- reset-password.js
+++ reset-password.js
@@ -1,7 +1,7 @@
 document.getElementById("reset-password-form").addEventListener("submit", (e) => {
     e.preventDefault();
     const token = new URLSearchParams(window.location.search).get("token");
-    fetch("https://play.retro-mmo.com/reset-password", {
+    fetch("https://retro-mmo.com/reset-password", {
         body: JSON.stringify({
             password: document.getElementById("password").value,
             token
@@ -11,13 +11,18 @@ document.getElementById("reset-password-form").addEventListener("submit", (e) =>
         },
         method: "POST"
     }).then(({ status }) => {
-        switch (status) {
-            case 200:
-                alert("Your password has been reset!");
-                break;
-            case 401:
-                alert("Your password reset link is invalid or has expired.");
-                break;
+        if (String(status).startsWith("5")) {
+            alert("The request could not be completed");
+        }
+        else {
+            switch (status) {
+                case 200:
+                    alert("Your password has been reset!");
+                    break;
+                case 401:
+                    alert("Your password reset link is invalid or has expired.");
+                    break;
+            }
         }
         location.replace("./");
     });
