4eb2ce2714cf791b92e389c186357e9af735d9ba
Evan Norton <evan@retro-mmo.com>
2023-12-09 01:12:41 -0500

    Show retrogaiden and retrommo player counts

--- global.js
+++ global.js
@@ -1,4 +1,7 @@
-fetch("https://play.retro-mmo.com/players.json").then((res) => res.json()).then((players) => {
-    const one = players.length === 1;
-    document.getElementById("players").innerText = `There ${one ? "is" : "are"} currently ${players.length.toLocaleString()} player${one ? "" : "s"} online!`;
+fetch("https://play.retro-mmo.com/players.json").then((res) => res.json()).then((players1) => {
+    fetch("https://play.retrogaiden.com/players.json").then((res) => res.json()).then((players2) => {
+        const players = [...players1, ...players2];
+        const one = players.length === 1;
+        document.getElementById("players").innerText = `There ${one ? "is" : "are"} currently ${players.length.toLocaleString()} player${one ? "" : "s"} online!`;
+    });
 });
