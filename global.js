289d31c676fd08ceda78f90741880a0cad304c61
Evan Norton <enorton@citystateentertainment.com>
2024-01-05 13:13:56 -0500

    Only show player count if more than 0

--- global.js
+++ global.js
@@ -1,7 +1,9 @@
 fetch("https://play.retro-mmo.com/players.json").then((res) => res.json()).then((players1) => {
     fetch("https://play.retrogaiden.com/players.json").then((res) => res.json()).then((players2) => {
         const players = [...players1, ...players2];
-        const one = players.length === 1;
-        document.getElementById("players").innerText = `There ${one ? "is" : "are"} currently ${players.length.toLocaleString()} player${one ? "" : "s"} online!`;
+        if (players.length > 0) {
+            const one = players.length === 1;
+            document.getElementById("players").innerText = `There ${one ? "is" : "are"} currently ${players.length.toLocaleString()} player${one ? "" : "s"} online!`;
+        }
     });
 });
