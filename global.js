79ee613fab99d311d47ca4e2603b9e8f09cfaa3a
Evan Norton <enorton@citystateentertainment.com>
2024-04-11 11:13:08 -0400

    Remove RetroGaiden

--- global.js
+++ global.js
@@ -1,9 +1,6 @@
-fetch("https://play.retro-mmo.com/players.json").then((res) => res.json()).then((players1) => {
-    fetch("https://play.retrogaiden.com/players.json").then((res) => res.json()).then((players2) => {
-        const players = [...players1, ...players2];
-        if (players.length > 0) {
-            const one = players.length === 1;
-            document.getElementById("players").innerText = `There ${one ? "is" : "are"} currently ${players.length.toLocaleString()} player${one ? "" : "s"} online!`;
-        }
-    });
+fetch("https://play.retro-mmo.com/players.json").then((res) => res.json()).then((players) => {
+    if (players.length > 0) {
+        const one = players.length === 1;
+        document.getElementById("players").innerText = `There ${one ? "is" : "are"} currently ${players.length.toLocaleString()} player${one ? "" : "s"} online!`;
+    }
 });
