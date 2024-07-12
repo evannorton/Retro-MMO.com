d4688cc3736330d45c5393edae5f532c9ed2e8f4
Evan Norton <evan@retro-mmo.com>
2024-07-12 01:22:12 -0500

    Update buttons

--- leaderboards.js
+++ leaderboards.js
@@ -1,22 +1,23 @@
 const urlParams = new URLSearchParams(window.location.search);
-const page = parseInt(urlParams.get('page')) || 1;
+const page = parseInt(urlParams.get("page")) || 1;
 
-fetch("http://localhost:3000/constants.json").then((res) => {
+fetch("https://play.retro-mmo.com/constants.json").then((res) => {
     return res.json();
 }).then((constants) => {
-    const entries = constants['leaderboards-entries'];
-    const entriesPerPage = constants['leaderboards-entries-per-page'];
+    const entries = constants["leaderboards-entries"];
+    const entriesPerPage = constants["leaderboards-entries-per-page"];
     const lastPage = Math.ceil(entries / entriesPerPage);
     console.log(lastPage);
 
     const createPagination = () => {
-        const pagination = document.createElement('div');
-        pagination.id = 'pagination';
+        const pagination = document.createElement("div");
+        pagination.id = "pagination";
 
         if (page > 1) {
-            const prevButton = document.createElement('button');
-            prevButton.innerHTML = '&#8592;'; // Arrow symbol
-            prevButton.className = 'pagination-button';
+            const prevButton = document.createElement("button");
+            prevButton.id = "prev-button";
+            prevButton.innerHTML = "&#8592;"; // Arrow symbol
+            prevButton.className = "pagination-button";
             prevButton.onclick = () => {
                 window.location.search = `?page=${page - 1}`;
             };
@@ -24,28 +25,28 @@ fetch("http://localhost:3000/constants.json").then((res) => {
         }
 
         if (page < lastPage) {
-            const nextButton = document.createElement('button');
-            nextButton.id = 'next-button';
-            nextButton.innerHTML = '&#8594;';
-            nextButton.className = 'pagination-button';
+            const nextButton = document.createElement("button");
+            nextButton.id = "next-button";
+            nextButton.innerHTML = "&#8594;";
+            nextButton.className = "pagination-button";
             nextButton.onclick = () => {
                 window.location.search = `?page=${page + 1}`;
             };
             pagination.appendChild(nextButton);
         }
 
-        const pageInputLabel = document.createElement('label');
-        pageInputLabel.innerText = 'Page: ';
-        pageInputLabel.setAttribute('for', 'page-input');
-        pageInputLabel.className = 'pagination-label';
+        const pageInputLabel = document.createElement("label");
+        pageInputLabel.innerText = "Page: ";
+        pageInputLabel.setAttribute("for", "page-input");
+        pageInputLabel.className = "pagination-label";
 
-        const pageInput = document.createElement('input');
-        pageInput.id = 'page-input';
-        pageInput.type = 'number';
+        const pageInput = document.createElement("input");
+        pageInput.id = "page-input";
+        pageInput.type = "number";
         pageInput.min = 1;
         pageInput.max = lastPage;
         pageInput.value = page;
-        pageInput.className = 'pagination-input';
+        pageInput.className = "pagination-input";
         pageInput.onchange = () => {
             const inputPage = parseInt(pageInput.value);
             if (inputPage > 0) {
@@ -55,20 +56,20 @@ fetch("http://localhost:3000/constants.json").then((res) => {
 
         pagination.appendChild(pageInputLabel);
         pagination.appendChild(pageInput);
-        document.querySelector('main').appendChild(pagination);
+        document.querySelector("main").appendChild(pagination);
     };
 
     const updateLeaderboards = () => {
         fetch(`https://play.retro-mmo.com/leaderboards.json?page=${page}`)
             .then((res) => {
                 if (!res.ok) {
-                    throw new Error('No more results');
+                    throw new Error("No more results");
                 }
                 return res.json();
             })
             .then((rows) => {
                 const tbody = document.querySelector("table tbody");
-                tbody.innerHTML = ''; // Clear previous rows
+                tbody.innerHTML = ""; // Clear previous rows
                 rows.forEach(({ experience, username }, key) => {
                     const tr = document.createElement("tr");
                     const td1 = document.createElement("td");
@@ -84,27 +85,26 @@ fetch("http://localhost:3000/constants.json").then((res) => {
                 });
 
                 if (page === 1) {
-                    const rowsArray = Array.from(tbody.querySelectorAll('tr'));
-                    if (rowsArray[0]) rowsArray[0].children[1].style.color = '#ffbb31'; // Gold
-                    if (rowsArray[1]) rowsArray[1].children[1].style.color = '#a8a8a8'; // Silver
-                    if (rowsArray[2]) rowsArray[2].children[1].style.color = '#ad4e1a'; // Bronze
+                    const rowsArray = Array.from(tbody.querySelectorAll("tr"));
+                    if (rowsArray[0]) rowsArray[0].children[1].style.color = "#ffbb31"; // Gold
+                    if (rowsArray[1]) rowsArray[1].children[1].style.color = "#a8a8a8"; // Silver
+                    if (rowsArray[2]) rowsArray[2].children[1].style.color = "#ad4e1a"; // Bronze
                 }
 
                 document.getElementById("leaderboards").removeAttribute("hidden");
             })
             .catch((error) => {
-                if (error.message === 'No more results') {
-                    const message = document.createElement('p');
-                    message.innerText = 'No more results';
-                    message.style.color = '#ffe737';
-                    const mainElement = document.querySelector('main');
-                    mainElement.insertBefore(message, document.getElementById('pagination'));
-                    document.getElementById('next-button').innerText = 'Back to start';
-                    document.getElementById('next-button').onclick = () => {
-                        window.location.search = `?page=1`;
+                if (error.message === "No more results") {
+                    const message = document.createElement("p");
+                    message.innerText = "No more results";
+                    message.style.color = "#ffe737";
+                    const mainElement = document.querySelector("main");
+                    mainElement.insertBefore(message, document.getElementById("pagination"));
+                    document.getElementById("prev-button").onclick = () => {
+                        window.location.search = "?page=1";
                     };
                 } else {
-                    console.error('An error occurred:', error);
+                    console.error("An error occurred:", error);
                 }
             });
     }
